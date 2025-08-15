78272e8d66d1ab1843c95c32b9cdfaa33f28b9d9
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-15 19:35:38 +0530

    Update dbo.sql

--- dbo.sql
+++ dbo.sql
@@ -1,14 +1,1 @@
-﻿CREATE TABLE [dbo].['Product Compare$23'](
-	[Test Case ID] [nvarchar](255) NULL,
-	[Test Scenario ] [nvarchar](255) NULL,
-	[Test Case Title] [nvarchar](255) NULL,
-	[Pre-requisites] [nvarchar](255) NULL,
-	[Test Steps] [nvarchar](max) NULL,
-	[Test Data] [nvarchar](255) NULL,
-	[Expected Result (ER)] [nvarchar](max) NULL,
-	[Actual Result] [nvarchar](255) NULL,
-	[Priority] [nvarchar](255) NULL,
-	[Result] [nvarchar](255) NULL,
-	[Comments] [nvarchar](255) NULL
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+select 'Test'
