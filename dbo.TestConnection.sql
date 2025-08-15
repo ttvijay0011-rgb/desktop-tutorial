f18081912e39d2d6f7b8775c4d6b7451ca55d71d
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-15 19:17:28 +0530

    Update dbo.TestConnection.sql

--- dbo.TestConnection.sql
+++ dbo.TestConnection.sql
@@ -1,6 +1,8 @@
 ALTER PROCEDURE [dbo].[TestConnection]
 AS
 BEGIN
+
+    -- Added
     SET NOCOUNT ON;
     SELECT 'Database Connection Successful' AS TestMessage,
            GETDATE() AS CurrentServerTime;
