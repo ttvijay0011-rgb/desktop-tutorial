bc848437c4d0e972b73c3d696e17d4aac6408a06
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-15 14:05:26 +0530

    vijay dbo.TestCRUD.sql

--- dbo.TestCRUD.sql
+++ dbo.TestCRUD.sql
@@ -1,7 +1,8 @@
-﻿ALTER PROCEDURE [dbo].[TestCRUD]
+CREATE OR ALTER PROCEDURE [dbo].[TestCRUD]
 AS
 BEGIN
-     ----Select 'Test' Vijay
+
+     -- Added by Vijay
     SET NOCOUNT ON;
 
     PRINT '--- CREATE ---';
@@ -19,11 +20,4 @@ BEGIN
     PRINT '--- DELETE ---';
     DELETE FROM dbo.TestTable
     WHERE Name LIKE 'CRUD_User%';
-
 END;
-GO
-/****** Object:  StoredProcedure [dbo].[TestInsertData]    Script Date: 15-08-2025 12:06:09 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
