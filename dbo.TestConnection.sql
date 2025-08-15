ef739abcb28feccaa126d93bc5aa54f1f90f5726
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-15 19:15:50 +0530

    Update dbo.TestConnection.sql

--- dbo.TestConnection.sql
+++ dbo.TestConnection.sql
@@ -1,14 +1,8 @@
-﻿ALTER PROCEDURE [dbo].[TestConnection]
+ALTER PROCEDURE [dbo].[TestConnection]
 AS
 BEGIN
     SET NOCOUNT ON;
---selected
     SELECT 'Database Connection Successful' AS TestMessage,
            GETDATE() AS CurrentServerTime;
 END;
-GO
-/****** Object:  StoredProcedure [dbo].[TestCRUD]    Script Date: 15-08-2025 12:06:09 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
 GO
