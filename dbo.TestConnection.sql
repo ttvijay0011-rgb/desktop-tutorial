708532c121e830ed28c72434c7b2bf4bf7ec321a
VijayKavi <vijayakumarthangavel@histogenetics.com>
2025-08-17 13:13:58 +0530

    removed some comments

--- dbo.TestConnection.sql
+++ dbo.TestConnection.sql
@@ -1,9 +1,7 @@
 ALTER PROCEDURE [dbo].[TestConnection]
 AS
 BEGIN
-
-
-    select 'Multiline123'
+ 
     SET NOCOUNT ON;
     SELECT 'Database Connection Successful' AS TestMessage,
            GETDATE() AS CurrentServerTime;
