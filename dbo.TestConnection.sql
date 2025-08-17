fe058e5993d7b0c3bfbe683680fff30978d52cfb
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-17 22:48:57 +0530

    corrected SP

--- dbo.TestConnection.sql
+++ dbo.TestConnection.sql
@@ -2,8 +2,8 @@ create or alter PROCEDURE [dbo].[TestConnection]
 AS
 BEGIN
 
-main 
- select 'Vijay added  '
+  
+   select 'Vijay added  '
     SET NOCOUNT ON;
     SELECT 'Database Connection Successful' AS TestMessage,
            GETDATE() AS CurrentServerTime;
