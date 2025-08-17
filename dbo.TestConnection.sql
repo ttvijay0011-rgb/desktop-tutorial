40027b054174819f43cc9fc4893d47365217f2db
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-17 23:30:21 +0530

    Dev_Setup

--- dbo.TestConnection.sql
+++ dbo.TestConnection.sql
@@ -1,7 +1,8 @@
-create PROCEDURE [dbo].[TestConnection]
+create or Alter PROCEDURE [dbo].[TestConnection]
 AS
 BEGIN
- 
+  
+  Select 'Dev Deployment'
     SET NOCOUNT ON;
     SELECT 'Database Connection Successful' AS TestMessage,
            GETDATE() AS CurrentServerTime;
