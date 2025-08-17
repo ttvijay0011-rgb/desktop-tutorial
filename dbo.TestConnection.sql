0b32d3f61d264fdcc22332190ee28c81b938b3f7
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-17 21:54:14 +0530

    added comments

--- dbo.TestConnection.sql
+++ dbo.TestConnection.sql
@@ -1,7 +1,9 @@
-create PROCEDURE [dbo].[TestConnection]
+create or alter PROCEDURE [dbo].[TestConnection]
 AS
 BEGIN
- 
+
+main 
+ select 'Vijay'
     SET NOCOUNT ON;
     SELECT 'Database Connection Successful' AS TestMessage,
            GETDATE() AS CurrentServerTime;
