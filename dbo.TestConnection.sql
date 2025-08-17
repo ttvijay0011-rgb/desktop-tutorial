a203622dd0374b621437feecd0402b9ffe69704f
ttvijay0011-rgb <ttvijay0011@gmail.com>
2025-08-18 00:03:33 +0530

    new

--- dbo.TestConnection.sql
+++ dbo.TestConnection.sql
@@ -3,7 +3,7 @@ AS
 BEGIN
 
   
-   select 'Vijay added   '
+   select 'Vijay added    newly'
     SET NOCOUNT ON;
     SELECT 'Database Connection Successful' AS TestMessage,
            GETDATE() AS CurrentServerTime;
